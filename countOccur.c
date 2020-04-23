e1917f97bc1e82e6ce2108278fade987e63bb533
elmedinkarisik <edo.karisik1@gmail.com>
2020-04-23 18:39:10 +0200

    implement countOccur

--- countOccur.c
+++ countOccur.c
@@ -1,58 +1,75 @@
+/* C program to count occurrences of a word in file */
+
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
-#define MAXCHAR 100000
- 
+#include <string.h>
+
+#define BUFFER_SIZE 1000
+
+/* Function declarations */
+int countOccurrences(FILE * file, const char* word);
+
+
 int main(int argc, char** argv) {
-    char unit[20], string[100000];
-    int i = 0, j = 0, count = 0;
+    FILE * file;
 
-    /* FILE * file; */
+    char word[50];
 
-    /* Open source files in 'r' mode */
+    int wCount;
+
+    /* Input word to search in file */
+    printf("Enter word to search in file: ");
+    scanf("%s", word);
+
+    /* Try to open file */
     file = fopen(argv[1], "r");
 
-
-    /* Check if file opened successfully */
-    /* if (file == NULL)
+    /* Exit if file not opened successfully */
+    if (file == NULL)
     {
-        printf("\nUnable to open file or no file is passed.\n");
-        printf("Please check if file exists and you have read privilege.\n");
+        printf("Unable to open file.\n");
+        printf("Please check you have read/write previleges.\n");
 
         exit(EXIT_FAILURE);
-    } */
- 	
-    /* i = 0;
-    do
-    {
-        fflush(argv[2]);
-        c = getchar();
-        string[i++] = c;
- 
-    } while (c != '\n');
-    string[i - 1] = '\0'; */
-    /* printf("Enter the word you want to find: "); */
-    fgets(string, MAXCHAR, argv[1]);
-    /* string = argv[1]; */
-    for (i = 0; i < strlen(string); i++)
-    {
-        while (i < strlen(string) && !isspace(string[i]) && isalnum(string[i]))
-        {
-            unit[j++] = string[i++];
-        }
-        if (j != 0)
-        {
-            unit[j] = '\0';
-            if (strcmp(unit, argv[2]) == 0)
-            {
-                count++;
-            }
-            j = 0;
-        }
     }
- 
-    printf("The number of times the word '%s' found in '%s' is '%d'.\n", argv[2], string, count);
+
+    // Call function to count all occurrence of word
+    wCount = countOccurrences(file, word);
+
+    printf("'%s' is found %d times in file.\n", word, wCount);
+
+
+    // Close file
+    fclose(file);
 
     return 0;
+}
+
+
+/* Returns total occurrences of a word in given file */
+int countOccurrences(FILE * file, const char* word)
+{
+    char str[BUFFER_SIZE];
+    char *pos;
+
+    int index, count;
+    
+    count = 0;
+
+    // Read line from file till end of file.
+    while ((fgets(str, BUFFER_SIZE, file)) != NULL)
+    {
+        index = 0;
+
+        // Find next occurrence of word in str
+        while ((pos = strstr(str + index, word)) != NULL)
+        {
+            /* Index of word in str is memory address of pos - memory address of str. */
+            index = (pos - str) + 1;
+
+            count++;
+        }
+    }
+
+    return count;
 }
